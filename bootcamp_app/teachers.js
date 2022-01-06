30115fb979e5bf14baa4953aec87976d9c521121
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-06 01:04:18 -0500

    Node Postgres completed.

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = student_id
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
