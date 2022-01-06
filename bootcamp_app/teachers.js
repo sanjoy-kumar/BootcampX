a87c7a96f061989de4d0b78dca3b75b994ec8fed
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-06 01:39:19 -0500

    SQL Injection - Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,38 @@ const pool = new Pool({
 });
 
 
-pool.query(`
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = students.cohort_id
+// JOIN assistance_requests ON students.id = student_id
+// JOIN teachers ON teachers.id = teacher_id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.cohort}: ${user.teacher}`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
+
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
 JOIN assistance_requests ON students.id = student_id
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
